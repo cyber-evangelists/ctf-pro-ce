ebaf0393869895f091968512d07417469204822d
Muhammad Husnain <bitsol44@gmail.com>
2024-02-20 16:17:02 +0500

    Update hash_pass.py

--- hash_pass.py
+++ hash_pass.py
@@ -7,7 +7,7 @@ def hash_password(password):
 
 if __name__ == "__main__":
     # Assumed password
-    password = "MySecretPassword"
+    password = "#ExamplePassword"
 
     hashed_password = hash_password(password)
     print("Hashed password:", hashed_password)
