8deb2cbc7ba3bf985d7ab276e276340111430276
Muhammad Husnain <bitsol44@gmail.com>
2024-02-20 16:16:22 +0500

    Delete ZIP directory

--- ZIP/config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-os.getenv
-USERNAME = os.getenv("U_NAME")
-PASSWORD = os.getenv("PASSWORD")
-
-print(USERNAME, PASSWORD)
--- ZIP/services/test.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-if sys.platform.startswith("linux"):
-    print("linux")
-elif os.name == "nt":
-    print("windows")
